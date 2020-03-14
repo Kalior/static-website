d028150c240ae2a86db672b1c50a59c7a4d72bc1
Joel Gustafsson <me@joelgustafsson.com>
2020-03-14 22:48:35 +0100

    Update site

--- js/application.js
+++ js/application.js
@@ -10,31 +10,42 @@ async function animateSubtitle() {
   let character_height = subtitleElement.height();
   subtitleElement.height(character_height);
 
-  subtitleElement.text("");
+  subtitleElement.text(" ");
 
-  let sentences = ["Software engineer.", "Musician.", "Photographer.", "Programmer."]
+  let parts = ["Bioinformatician ", "and Software engineer."]
 
+
+  await idle(subtitleElement, 1);
+  for (let word of parts) {
+    await idle(subtitleElement, 2);
+    await write_word(word, subtitleElement);
+  }
+}
+
+async function write_word(word, element) {
+  for (let character of word) {
+    element.append(character);
+    element.append("|");
+    await sleep(20 + Math.random() * 30);
+    element.text(element.text().slice(0, -1));
+  }
+}
+
+
+async function erase_word(word, element) {
+  for (let character of word.split("").reverse()) {
+    element.text(element.text().slice(0, -1));
+    await sleep(40 + Math.random() * 60);
+  }
   await sleep(500);
-  while (true) {
-    let sentence = sentences[Math.floor(Math.random() * sentences.length)];
+}
 
-    let width = sentence.length * character_width + 1;
-    subtitleElement.width(width);
-
-    for (let character of sentence) {
-      subtitleElement.append(character);
-      if (character != " ") {
-        await sleep(40 + Math.random() * 60);
-      }
-    }
-
-    await sleep(1000);
-
-    for (let character of sentence.split("").reverse()) {
-      subtitleElement.text(subtitleElement.text().slice(0, -1));
-      await sleep(40 + Math.random() * 60);
-    }
-    await sleep(500);
+async function idle(element, iterations) {
+  for (let i = 0; i < iterations; i++) {
+    element.append("|");
+    await sleep(210);
+    element.text(element.text().slice(0, -1));
+    await sleep(210);
   }
 }
 
